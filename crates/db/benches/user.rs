4c3198557e64a4449700f8217165ab0d3e897244
Liam <31192478+terror@users.noreply.github.com>
2021-02-17 22:32:14 -0500

    Add user creation benchmark

---
+++ crates/db/benches/user.rs
@@ -0,0 +1,26 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use db::Db;
+use twilight_model::id::UserId;
+
+async fn benchmark(db: &Db, id: u64) {
+  let user_id = UserId(id);
+  db.user(user_id).await.unwrap();
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+  let runtime = tokio::runtime::Builder::new_multi_thread()
+    .enable_all()
+    .build()
+    .unwrap();
+  let db = runtime.block_on(async { Db::new().await.unwrap() });
+  let mut id = 0;
+  c.bench_with_input(BenchmarkId::new("benchmark", 1000), &db, |b, s| {
+    b.to_async(&runtime).iter(|| {
+      benchmark(&s, id);
+      id += 1;
+    })
+  });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
