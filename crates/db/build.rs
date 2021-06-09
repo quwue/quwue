29d5f97b51e42d5da12298aef3f8b0a8955d58ae
Casey Rodarmor <casey@rodarmor.com>
2021-06-09 16:45:49 -0700

    Run CI on Windows (#19)

--- crates/db/build.rs
+++ crates/db/build.rs
@@ -1,10 +1,17 @@
+use camino::Utf8PathBuf;
 use sqlx::{migrate::MigrateDatabase, Sqlite, SqlitePool};
-use std::{env, fs, path::Path};
+use std::{convert::TryFrom, env, fs, path::Path};
 
 #[tokio::main]
 async fn main() {
   let db_path = Path::new(&env::var_os("OUT_DIR").unwrap()).join("db.sqlite");
-  let db_url = format!("sqlite://{}", db_path.to_str().unwrap());
+  let db_path = Utf8PathBuf::try_from(db_path).unwrap();
+
+  let db_url = if cfg!(windows) {
+    format!("sqlite:///{}", db_path.as_str().replace('\\', "\\\\"))
+  } else {
+    format!("sqlite:{}", db_path)
+  };
 
   Sqlite::create_database(&db_url).await.unwrap();
 
