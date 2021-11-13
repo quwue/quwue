d975ba82fa1ab4b2a1d471d77afc3b59e19ea507
Casey Rodarmor <casey@rodarmor.com>
2021-11-12 19:08:02 -0800

    Use BIGSERIAL instead of AUTOINCREMENT

--- crates/db/migrations/3_prompts.sql
+++ crates/db/migrations/3_prompts.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS prompts (
-  id                   INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  discriminant         INTEGER NOT NULL,
-  message_id           INTEGER NOT NULL,
-  payload              INTEGER,
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  discriminant INTEGER NOT NULL,
+  message_id INTEGER NOT NULL,
+  payload INTEGER,
   recipient_discord_id INTEGER NOT NULL UNIQUE,
   FOREIGN KEY (recipient_discord_id) references users(discord_id)
 );
